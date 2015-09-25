0240ecb58f3ad744c2e54f16e794e4fede4bd963
Mariooo2 <marioa_bros@hotmail.com>
2015-09-24 20:18:16 -0500

    Update Direccion.java

--- Direccion.java
+++ Direccion.java
@@ -1,3 +1,20 @@
-class Direccion{
-	
+class DireccionP{
+	private String delegacion,colonia;
+
+        public void CapturaDireccion(){
+          LeeDelegacion();
+          LeeColonia();
+         }
+
+        public void LeeDelegacion(){
+         delegacion=Teclado.LeeCadena("Delegacion: ");
+         }
+
+        public void LeeColonia(){
+         colonia=Teclado.LeeCadena("Colonia: ");
+         }
+
+        public void Listar(){
+         System.out.println(delegacion+"\t"+colonia);
+         }
 }
