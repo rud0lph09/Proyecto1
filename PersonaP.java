4f45ff3006238c7a156fd9f7de547f0adc243be9
Mariooo2 <marioa_bros@hotmail.com>
2015-09-29 18:20:34 -0500

    Update PersonaP.java

--- PersonaP.java
+++ PersonaP.java
@@ -1,6 +1,5 @@
 class PersonaP{
-private int edad;
-private String genero,estudios,ocupacion, idioma;
+private int edad, genero,estudios,ocupacion, idioma;
 
 public void CapturaPersona(){
 LeeEdad();
@@ -12,21 +11,24 @@ public void LeeEdad(){
 edad=Teclado.LeeEntero("Edad: ");
 }
 public void LeeGenero(){
-genero=Teclado.LeeCadena("Genero: ");
+genero=Teclado.LeeEntero("Usted es: \n1.-Hombre\n2.-Mujer\n");
 }
 public void LeeEstudios(){
-estudios=Teclado.LeeCadena("Nivel maximo de estudios: ");
+estudios=Teclado.LeeEntero("Nivel maximo de estudios:\n1.-Primaria\n2.-Secundaria\n3.-Preparatoria\n4.-Licenciatura\n5.-Otro\n ");
 }
 public void LeeOcupacion(){
-ocupacion=Teclado.LeeCadena("Trabaja o Estudia: ");
+ocupacion=Teclado.LeeEntero("¿A que se dedica?\n1.-Estudia\n2.-Trabaja\n");
 }
 public void Listar(){
 System.out.print(edad+"\t"+genero+"\t"+estudios+"\t"+ocupacion);
 }
 public void LeeIdioma(){
 int x;
-idioma=Teclado.LeeCadena("Aparte del Español, ¿hablas algun otro idioma?\n1.-Si\n2.-No\n ");
-if(idioma=="si" || idioma=="Si")
+idioma=Teclado.LeeEntero("Aparte del Español, ¿hablas algun otro idioma?\n1.-Si\n2.-No\n ");
+if(idioma==1)
 x=Teclado.LeeEntero("¿Cuantos?: ");
 }
+public int GetEdad(){
+   return edad;
+   }
 }
