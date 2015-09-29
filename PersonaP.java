66ef3168f34d612d92dc87d44a719559ccc9fe48
Mariooo2 <marioa_bros@hotmail.com>
2015-09-28 22:31:17 -0500

    Create PersonaP.java

---
+++ PersonaP.java
@@ -0,0 +1,38 @@
+class PersonaP{
+private int edad;
+private String genero,estudios,ocupacion, idioma;
+private DireccionP dir;
+
+public PersonaP(){
+
+}
+
+public void CapturaPersona(){
+LeeEdad();
+LeeGenero();
+LeeEstudios();
+LeeOcupacion();
+dir.CapturaDireccion();
+}
+public void LeeEdad(){
+edad=Teclado.LeeEntero("Edad: ");
+}
+public void LeeGenero(){
+genero=Teclado.LeeCadena("Genero: ");
+}
+public void LeeEstudios(){
+estudios=Teclado.LeeCadena("Nivel maximo de estudios: ");
+}
+public void LeeOcupacion(){
+ocupacion=Teclado.LeeCadena("Trabaja o Estudia: ");
+}
+public void Listar(){
+System.out.print(edad+"\t"+genero+"\t"+estudios+"\t"+ocupacion);
+}
+public void LeeIdioma(){
+int x;
+idioma=Teclado.LeeCadena("Aparte del Español, ¿hablas algun otro idioma?\n1.-Si\n2.-No\n ");
+if(idioma=="si" || idioma=="Si")
+x=Teclado.LeeEntero("¿Cuantos?: ");
+}
+}
